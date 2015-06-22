1d7e30d3cac4fadbfd372f012f2f69bd180b2e94
Claire McEnery <brian.m.small@gmail.com>
2015-06-22 11:16:28 -0500

    Added mime types

---
+++ server.js
@@ -0,0 +1,9 @@
+var http = require('http');
+var url = require('url');
+var path = require('path');
+var fs = require('fs');
+
+var mimeTypes = {
+  "html" : "text/html",
+  "jpeg" : "image/jpeg"
+}
